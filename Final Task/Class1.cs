2c6b1726495dde4e2b1aea35971ca22aa27321b8
Anton Kaznodii <kkaznatushka@gmail.com>
2024-08-23 14:27:29 +0300

    BDD soon

--- Final Task/Class1.cs
+++ Final Task/Class1.cs
@@ -1,20 +1,18 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using log4net.Config;
+using log4net;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
-using FluentAssertions;
-using log4net;
-using log4net.Config;
-using System;
-using System.IO;
 
 namespace WebDriver3
 {
-    [TestFixture]
-    [Parallelizable(ParallelScope.All)]
-    public class WebDriverTests
+    public class TestBase
     {
-        private static readonly ILog log = LogManager.GetLogger(typeof(WebDriverTests));
+        protected IWebDriver driver;
+        protected static readonly ILog log = LogManager.GetLogger(typeof(TestBase));
 
         [OneTimeSetUp]
         public void SetupLogging()
@@ -23,97 +21,111 @@ namespace WebDriver3
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
         }
 
+        [SetUp]
+        public void InitDriver()
+        {
+            string browser = TestContext.Parameters.Get("browser", "Edge");
+
+            switch (browser.ToLower())
+            {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    throw new ArgumentException($"Unsupported browser: {browser}");
+            }
+
+            log.Info($"Driver initialized: {browser}");
+        }
+
+        [TearDown]
+        public void CloseDriver()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+            log.Info("Driver closed.");
+        }
+    }
+    public class LoginPage
+    {
+        private readonly IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement TxtName => driver.FindElement(By.CssSelector("#user-name"));
+        private IWebElement TxtPass => driver.FindElement(By.CssSelector("#password"));
+        private IWebElement ErrorMessage => driver.FindElement(By.CssSelector("h3[data-test='error']"));
+
+        public void EnterUsername(string username)
+        {
+            TxtName.SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            TxtPass.SendKeys(password);
+        }
+
+        public void Submit()
+        {
+            TxtPass.SendKeys(Keys.Enter);
+        }
+
+        public string GetErrorMessage()
+        {
+            return ErrorMessage.Text;
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class WebDriverTests : TestBase
+    {
+        [Test]
+        [TestCase("standard_user", "secret_sauce", "https://www.saucedemo.com/inventory.html", TestName = "Test3 - Valid Login")]
+        public void HappyLoginTest(string username, string password, string expectedUrl)
+        {
+            ExecuteLoginTest(username, password);
+            ValidateUrl(expectedUrl);
+        }
+
         [Test]
         [TestCase("", "anypassword", "Username is required", TestName = "Test1 - Empty Username")]
         [TestCase("anyuser", "", "Password is required", TestName = "Test2 - Empty Password")]
-        [TestCase("standard_user", "secret_sauce", "Swag Labs", TestName = "Test3 - Valid Login")]
-        public void ChromeLoginTests(string username, string password, string expectedMessage)
+        public void NegativeLoginTest(string username, string password, string expectedMessage)
         {
-            IWebDriver driver = new ChromeDriver();
-            ExecuteLoginTest(driver, username, password, expectedMessage);
+            ExecuteLoginTest(username, password);
+            ValidateErrorMessage(expectedMessage);
         }
 
-        [Test]
-        [TestCase("", "anypassword", "Username is required", TestName = "Test1 - Empty Username")]
-        [TestCase("anyuser", "", "Password is required", TestName = "Test2 - Empty Password")]
-        [TestCase("standard_user", "secret_sauce", "Swag Labs", TestName = "Test3 - Valid Login")]
-        public void EdgeLoginTests(string username, string password, string expectedMessage)
+        private void ExecuteLoginTest(string username, string password)
         {
-            IWebDriver driver = new EdgeDriver();
-            ExecuteLoginTest(driver, username, password, expectedMessage);
+            var loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage.EnterUsername(username);
+            loginPage.EnterPassword(password);
+            loginPage.Submit();
         }
 
-        private void ExecuteLoginTest(IWebDriver driver, string username, string password, string expectedMessage)
+        private void ValidateErrorMessage(string expectedMessage)
         {
-            try
-            {
-                log.Info("Starting test with Username: " + username + " and Password: " + password);
-
-                // Given: The user is on the login page
-                driver.Navigate().GoToUrl("https://www.saucedemo.com/");
-                driver.Manage().Window.Maximize();
-
-                // When: The user enters username and password
-                IWebElement txtName = driver.FindElement(By.CssSelector("#user-name"));
-                txtName.SendKeys(username);
-
-                IWebElement txtPass = driver.FindElement(By.CssSelector("#password"));
-                txtPass.SendKeys(password);
-
-                // And: The user submits the form
-                txtPass.SendKeys(Keys.Enter);
-
-                // Then: The system should display the appropriate message or redirect to the dashboard
-                if (string.IsNullOrEmpty(username))
-                {
-                    ValidateErrorMessage(driver, expectedMessage);
-                }
-                else if (string.IsNullOrEmpty(password))
-                {
-                    ValidateErrorMessage(driver, expectedMessage);
-                }
-                else
-                {
-                    ValidateTitle(driver, expectedMessage);
-                }
-            }
-            catch (Exception ex)
-            {
-                log.Error("Test failed with exception: ", ex);
-            }
-            finally
-            {
-                driver?.Quit();
-                driver?.Dispose();
-            }
+            var loginPage = new LoginPage(driver);
+            string actualMessage = loginPage.GetErrorMessage();
+            actualMessage.Should().Contain(expectedMessage);
+            log.Info($"Error message validated: '{expectedMessage}'");
         }
 
-        private void ValidateErrorMessage(IWebDriver driver, string expectedMessage)
+        private void ValidateUrl(string expectedUrl)
         {
-            try
-            {
-                IWebElement errorMessage = driver.FindElement(By.CssSelector("h3[data-test='error']"));
-                errorMessage.Text.Should().Contain(expectedMessage);
-                log.Info($"Error message validated: '{expectedMessage}'");
-            }
-            catch (NoSuchElementException ex)
-            {
-                log.Error("Error message not found.", ex);
-            }
-        }
-
-        private void ValidateTitle(IWebDriver driver, string expectedTitle)
-        {
-            try
-            {
-                string actualTitle = driver.Title;
-                actualTitle.Should().Be(expectedTitle);
-                log.Info($"Title validated: '{expectedTitle}'");
-            }
-            catch (NoSuchElementException ex)
-            {
-                log.Error("Title element not found.", ex);
-            }
+            string actualUrl = driver.Url;
+            actualUrl.Should().Be(expectedUrl);
+            log.Info($"URL validated: '{expectedUrl}'");
         }
     }
 }
